41030f353fc4cce59f2ca15c6201bedb89277afd
Feetu Nyrhinen <feetu.nyrhinen@gmail.com>
2019-11-19 15:26:10 +0100

    Call the mutation changePassword from the frontend with the changed password data and a show a confirmation dialog on success

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,14 @@
 import React, { useState } from 'react'
-import { Button, TextField } from '@material-ui/core'
+import { gql } from 'apollo-boost'
+import {
+  Button,
+  TextField,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions
+} from '@material-ui/core'
 import { makeStyles } from '@material-ui/styles'
 
 const useStyles = makeStyles({
@@ -18,10 +27,49 @@ const useStyles = makeStyles({
   }
 })
 
+const EDIT_USER = gql`
+  mutation editUser(
+    $id: ID!
+    $email: String!
+    $givenname: String
+    $surname: String
+  ) {
+    editUser(id: $id, email: $email, givenname: $givenname, surname: $surname) {
+      id
+      email
+      givenname
+      surname
+    }
+  }
+`
+
+const CHANGE_PASSWORD = gql`
+  mutation changePassword(
+    $currentPassword: String!
+    $newPassword: String!
+    $newPassword2: String!
+  ) {
+    changePassword(
+      currentPassword: $currentPassword
+      newPassword: $newPassword
+      newPassword2: $newPassword2
+    ) {
+      id
+      email
+      givenname
+      surname
+    }
+  }
+`
+
 const Profile = ({ show, client, user }) => {
   const [givenname, setGivenname] = useState('')
   const [surname, setSurname] = useState('')
   const [email, setEmail] = useState('')
+  const [currentPassword, setCurrentPassword] = useState('')
+  const [newPassword, setNewPassword] = useState('')
+  const [newPassword2, setNewPassword2] = useState('')
+  const [showSuccessDialog, setShowSuccessDialog] = useState(false)
   const classes = useStyles()
 
   if (!user) {
@@ -50,7 +98,8 @@ const Profile = ({ show, client, user }) => {
     setEmail(event.target.value)
   }
 
-  const handleSubmitClick = event => {
+  const handleSubmitClick = async event => {
+    event.preventDefault()
     console.log(
       'Submit clicked, updating the data [givenname:',
       givenname,
@@ -60,8 +109,56 @@ const Profile = ({ show, client, user }) => {
       email,
       ']'
     )
-    // TODO
-    //
+    // TODO the call to the backend
+  }
+
+  const handleCurrentPasswordChange = event => {
+    setCurrentPassword(event.target.value)
+  }
+
+  const handleNewPasswordChange = event => {
+    setNewPassword(event.target.value)
+  }
+
+  const handleNewPassword2Change = event => {
+    setNewPassword2(event.target.value)
+  }
+
+  const handleChangePasswordSubmit = async event => {
+    event.preventDefault()
+    console.log(
+      'Submit clicked, sending a request to change the password',
+      currentPassword,
+      newPassword,
+      newPassword2
+    )
+
+    try {
+      const { data, loading, error } = await client.mutate({
+        mutation: CHANGE_PASSWORD,
+        variables: {
+          currentPassword: currentPassword,
+          newPassword: newPassword,
+          newPassword2: newPassword2
+        }
+      })
+      if (data) {
+        console.log('Response data of changePassword', data)
+        if (data.changePassword !== null) {
+          handleDialogOpen()
+        }
+      }
+    } catch (e) {
+      console.log('error when changing a password', e)
+    }
+  }
+
+  const handleDialogOpen = () => {
+    setShowSuccessDialog(true)
+  }
+
+  const handleDialogClose = () => {
+    setShowSuccessDialog(false)
   }
 
   if (!show) {
@@ -100,11 +197,83 @@ const Profile = ({ show, client, user }) => {
         <br />
         <br />
         {/* Data last modified: {user.modified} */}
-        <Button color='primary' variant='contained' onClick={handleSubmitClick}>
+        <Button
+          id='submit_user_data_button'
+          color='primary'
+          variant='contained'
+          onClick={handleSubmitClick}
+        >
           {' '}
-          Update
+          Update user data
         </Button>
       </form>
+      <h3>Change password</h3>
+      <form>
+        <TextField
+          id='currentpassword_field'
+          variant='standard'
+          label='Current password: '
+          onChange={handleCurrentPasswordChange}
+          value={currentPassword}
+          className={classes.textField}
+          type='password'
+        />
+        <TextField
+          id='newpassword_field'
+          variant='standard'
+          label='New password: '
+          onChange={handleNewPasswordChange}
+          value={newPassword}
+          className={classes.textField}
+          type='password'
+        />
+        <TextField
+          id='newpassword2_field'
+          variant='standard'
+          label='New password confirmation: '
+          onChange={handleNewPassword2Change}
+          value={newPassword2}
+          className={classes.textField}
+          type='password'
+        />
+        <br />
+        <br />
+        <Button
+          id='submit_new_password_button'
+          color='default'
+          variant='contained'
+          onClick={handleChangePasswordSubmit}
+        >
+          Change password
+        </Button>
+      </form>
+
+      <Dialog open={showSuccessDialog} onClose={handleDialogClose}>
+        Password changed successfully.
+      </Dialog>
+
+      <Dialog
+        open={showSuccessDialog}
+        onClose={handleDialogClose}
+        aria-labelledby='alert-dialog-title'
+        aria-describedby='alert-dialog-description'
+      >
+        <DialogTitle id='alert-dialog-title'>Password changed</DialogTitle>
+        <DialogContent>
+          <DialogContentText id='alert-dialog-description'>
+            Password changed successfully.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            onClick={handleDialogClose}
+            color='primary'
+            variant='contained'
+          >
+            OK
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   )
 }
